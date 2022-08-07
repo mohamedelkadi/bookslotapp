eabccc9c1caef808a1f8bdb6c3762fb255c6918b
Mo <molhm66@gmail.com>
2022-08-07 20:47:30 +0200

    refactor book_slot_service to service

--- app/controllers/api/slots_controller.rb
+++ app/controllers/api/slots_controller.rb
@@ -12,38 +12,16 @@ module Api
     end
 
     def book
-      day = book_params['day']&.to_date.to_s
-      duration = book_params['duration'].to_i.minutes
-      slot = book_params['slot']
-
-      slot_id = SecureRandom.uuid
-      start_t = Time.parse "#{day} #{slot}"
-      end_t = start_t + duration
-
-      $mem_storage[day] ||= []
-
-      conflict = $mem_storage[day].any? do |slot|
-        new_ss, new_se = start_t.to_time, end_t.to_time
-        ss, se = slot[:start].to_time, slot[:end].to_time
-        inner_between?(new_se ,ss, se) || inner_between?(new_ss ,ss, se) || se == new_se || new_ss == ss
-      end
+      conflict, slot_id, day = BookSlotService.new(book_params).call
 
       if conflict
         return render json: { error: 'conflict' }, status: :unprocessable_entity
       end
 
-      $mem_storage[day] << { id: slot_id.to_s, start: start_t.to_s, end: end_t.to_s }.with_indifferent_access
-
-      puts "Memory dumb"
-      puts $mem_storage[day]
-      puts $mem_storage
-
       render json: { id: slot_id, day: day }
     end
 
-    def inner_between?(time, range_s, range_end)
-      time.between?(range_s + 1.minute, range_end - 1.minute)
-    end
+    private
 
     def list_params
       params.permit([:day, :duration_hours, :duration_minutes])
--- app/services/book_slot_service.rb
+++ app/services/book_slot_service.rb
@@ -1,2 +1,58 @@
 class BookSlotService
+  def initialize(book_params)
+    @book_params = book_params
+  end
+
+  def call
+    return [true, nil, nil] if conflict?
+
+    slot_id = SecureRandom.uuid
+
+    $mem_storage[day] ||= []
+
+    $mem_storage[day] << { id: slot_id.to_s, start: start_t.to_s, end: end_t.to_s }.with_indifferent_access
+
+    [false, slot_id, day]
+  end
+
+  private
+
+  def conflict?
+    @conflict ||= $mem_storage[day].any? do |slot|
+      new_ss, new_se = start_t.to_time, end_t.to_time
+      ss, se = slot[:start].to_time, slot[:end].to_time
+
+      inner_between?(new_se, ss, se) || inner_between?(new_ss, ss, se) || se == new_se || new_ss == ss
+    end
+  end
+
+  attr_reader :book_params
+
+  def slot
+    @slot ||= book_params['slot']
+  end
+
+  def end_t
+    start_t + duration
+  end
+
+  def start_t
+    @start_at ||= Time.parse "#{day} #{slot}"
+  end
+
+  def end_at
+    @end_at ||= start_t + duration
+  end
+
+  def duration
+    @duration ||= book_params['duration'].to_i.minutes
+  end
+
+  def inner_between?(time, range_s, range_end)
+    time.between?(range_s + 1.minute, range_end - 1.minute)
+  end
+
+  def day
+    @day ||= book_params['day']&.to_date.to_s
+  end
 end
